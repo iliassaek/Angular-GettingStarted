43649a9b19a0139adedd14ef8d63cccfe21077b1
felouss999@gmail.com <Iliass@@7>
2020-04-12 21:18:27 +0200

    ad product list component

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
   selector : 'pm-root' ,
   template : `
     <div><h1>{{pageTitle}}</h1>
-      <div>This makes a component<div>
+      <div>This makes a component</div>
     </div>
+    <pm-products></pm-products>
   `
 })
 export class AppComponent {

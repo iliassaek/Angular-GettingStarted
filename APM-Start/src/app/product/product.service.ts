59127b5d9664f18981187154772e9ff8a9c13b0c
felouss999@gmail.com <Iliass@@7>
2020-04-13 19:46:46 +0200

    add an injectable service

---
+++ APM-Start/src/app/product/product.service.ts
@@ -0,0 +1,29 @@
+import { IProduct } from './product';
+
+export class ProductService {
+    getProducts():IProduct[]{
+        return [
+            {
+                "productId": 5,
+                "productName": "Hammer",
+                "productCode": "TBX-0048",
+                "releaseDate": "May 21, 2019",
+                "description": "Curved claw steel hammer",
+                "price": 8.9,
+                "starRating": 4.8,
+                "imageUrl": "assets/images/hammer.png"
+              },
+              {
+                "productId": 8,
+                "productName": "Saw",
+                "productCode": "TBX-0022",
+                "releaseDate": "May 15, 2019",
+                "description": "15-inch steel blade hand saw",
+                "price": 11.55,
+                "starRating": 3.7,
+                "imageUrl": "assets/images/saw.png"
+              }
+
+        ]
+    }
+}

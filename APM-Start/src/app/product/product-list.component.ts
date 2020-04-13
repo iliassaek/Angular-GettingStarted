06eb6b485e635bd595bdb0a7e005f2baf1c99387
felouss999@gmail.com <Iliass@@7>
2020-04-13 19:39:45 +0200

    raise event from component to container

--- APM-Start/src/app/product/product-list.component.ts
+++ APM-Start/src/app/product/product-list.component.ts
@@ -70,4 +70,8 @@ export class ProductListComponent implements OnInit {
           (product:IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
         );
       }
+
+      onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List :' + message ;
+      }
 }
--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import { EventEmitter ,Component, OnChanges, Input, Output } from '@angular/core';
+
 
 @Component({
     selector : 'pm-star',
@@ -10,7 +11,14 @@ export class StarComponent implements OnChanges {
     @Input() rating : number;
     starWidth : number ;
 
+    @Output() ratingClicked : EventEmitter<string> = new EventEmitter<string>() ;
+
     ngOnChanges(): void {
         this.starWidth = this.rating*75/5;
+    }
+
+    onClick():void{
+        //rize the click event to the container
+        this.ratingClicked.emit(`the ratinf ${this.rating} was clicked`) ;
     }
 }
